3c0e410a6f1d88e3f4a732415b5b1ea6b947be1a
Christopher Breit <chrisbreit@Christophers-MacBook-Pro.local>
2018-02-24 11:20:02 -0700

    refactored service call file

--- app/common/Model/ServiceCall.js
+++ app/common/Model/ServiceCall.js
@@ -1,40 +1,15 @@
 import axios from 'axios';
-import { has, assign } from 'lodash';
 
-export const apiCall = ({ method, url, data = {}, success, error = false, acceptType = 'application/json', headers = {}}) => {
+export const apiCall = ({ method, url, data = {}, success, error = false }) => {
     return (dispatch, getState) => {
-        const contentType = method === 'GET' || method === 'DELETE' ? undefined : 'application/json';
-
-        const headersBase = {
-            Accept: acceptType,
-            // AccessToken: sessionStorage.token,
-            'Content-Type': contentType,
-        };
-
-        const fetchParams = {
-            url,
-            method,
-            headers: assign(headersBase, headers),
-            data,
-            success,
-            error
-        };
+        const fetchParams = { url, method, data, success, error };
 
         return dispatch(executeAPICall(fetchParams));
-    };
-};
 
-const executeAPICall = ({ method, url, data, headers, success, error }) => {
-    return (dispatch, getState) => {
-        const httpRequest = axios({ method, url, data, headers }).then((data) => {
-            const headers = data.headers ? data.headers.location : false,
-                responseData = data.data.Data || headers ? { Data: data.data.Data, Headers: data.headers } : data.data;
-
-            dispatch(success(responseData));
-        }).catch((err) => {
-            const e = has(err, 'response.data') ? err.response.data : err;
-
-            console.error(e);;
-        });
+        axios({ method, url, data }).then(
+            response => dispatch(success(response))
+        ).catch(
+            e => dispatch(error(e))
+        );
     };
 };
